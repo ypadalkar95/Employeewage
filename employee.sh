437e97731c603cef58114bc7bed574ab4a08cc92
Yogesh <ypadalkar95@gmail.com>
2023-02-04 23:08:43 +0530

    DailyEmployeewages

--- employee.sh
+++ employee.sh
@@ -1,11 +1,19 @@
 echo " Welcome to Employee wage computation "
 
-isPresent=$((RANDOM%2));
+isPresent=$((RANDOM%3));
+perHourSalary=20;
+workingHour=0;
 if [ $isPresent -eq 0 ]
 then
 	echo "Employee is absent";
+	workingHour=0;
 else
 	echo "Employee is present";
+	workingHour=8;
+
 fi
+salary=$(($perHourSalary * $workingHour));
+
+echo "Employee has earned $salary $ today";
 
 
