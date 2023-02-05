863a082a68d70b50ea4e92934dc1c3ba1e2cb904
Yogesh <ypadalkar95@gmail.com>
2023-02-05 11:36:57 +0530

    usingfunction

--- employee.sh
+++ employee.sh
@@ -1,29 +1,18 @@
-echo " Welcome to Employee wage computation "
-
-isPresent=$((RANDOM%3));
-perHourSalary=20;
-workingHour=0;
-Days=20
-case $isPresent in
-	0)
-	echo "Employee is absent";
-	workingHour=0;
-	;;
-
-	1)
-	echo "Employee is present";
-	workingHour=8;
-	;;
-
-	2)
-	echo "Employee is working as part time";
-	workingHour=4;
-	;;
-
-esac
-
-salary=$(($perHourSalary * $workingHour * $Days));
-
-echo "Employee has earned $salary ";
+function calculateWorkingHour() {
+	case $1 in
+		0)
+			workingHour=0;
+			;;
+		1)
+			workingHour=8;
+			;;
+		2)
+			workingHour=4;
+			;;
+	esac;
+	echo $workingHour;
+}
+	wHour=$(calculateWorkingHour $((RANDOM%3)));
+echo "Employee working hour is : $wHour"
 
 
