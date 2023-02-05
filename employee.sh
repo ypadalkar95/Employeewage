b412c2bc8aa9cc117033e7b3644fc705213e6a5b
Yogesh <ypadalkar95@gmail.com>
2023-02-05 11:47:38 +0530

    conditionofdayandhour

--- employee.sh
+++ employee.sh
@@ -12,7 +12,25 @@ function calculateWorkingHour() {
 	esac;
 	echo $workingHour;
 }
+perHourSalary=20;
+totalSalary=0;
+totalWorkingHour=0;
+day=1;
+
+while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
+do
 	wHour=$(calculateWorkingHour $((RANDOM%3)));
-echo "Employee working hour is : $wHour"
+	totalWorkingHour=$(($totalWorkingHour + $wHour));
+	if [ $totalWorkingHour -gt 100 ]
+then
+		totalWorkingHour=$(($totalWorkingHour - $wHour));
+		break;
+	fi
+	salary=$(($perHourSalary * $wHour));
+	totalSalary=$(($totalSalary + $salary));
+	((day++));
+done
+echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
+
 
 
